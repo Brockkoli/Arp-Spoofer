130241c2fe130bec86fc5ccd1c7ce4d74cd60963
Brockkoli <kianyun8186@gmail.com>
2022-10-11 20:03:43 +0800

    added netfilterqueue

--- requirements.sh
+++ requirements.sh
@@ -8,6 +8,14 @@ sudo apt-get install python3
 echo "Installing Scapy..."
 sudo apt install python3-scapy
 
+echo "Installing netfilterqueue..."
+sudo apt install libnetfilter-queue-dev
+pip install NetfilterQueue
+pip install cython
+git clone https://github.com/oremanj/python-netfilterqueue
+cd python-netfilterqueue
+pip install .
+
 echo "Installing pyfiglet..."
 sudo apt install python3-pyfiglet
 
